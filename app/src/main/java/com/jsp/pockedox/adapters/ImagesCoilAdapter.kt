16469627126ed6fb8409b8a1fada51298bd17785
vladdovgal <vladdovgal@gmail.com>
2020-10-08 16:50:54 +0300

    make images load from the internet

--- app/src/main/java/com/jsp/pockedox/adapters/ImagesCoilAdapter.kt
+++ app/src/main/java/com/jsp/pockedox/adapters/ImagesCoilAdapter.kt
@@ -3,11 +3,12 @@ package com.jsp.pockedox.adapters
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.api.load
+import com.jsp.pockedox.R
 
 @BindingAdapter("image")
 fun loadWithCoil(imageView: ImageView, imageUrl: String){
     imageView.load(imageUrl){
-        //todo image placeholder
-        // todo errorimage
+        placeholder(R.drawable.img_placeholder)
+        error(R.drawable.img_error)
     }
 }
