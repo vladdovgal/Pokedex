adae81e67ada32f0a84c4f0025b075afb60f3468
vladdovgal <vladdovgal@gmail.com>
2020-10-16 17:01:41 +0300

    add second activity

--- app/src/main/java/com/epam/pockedox/data/PokedexService.kt
+++ app/src/main/java/com/epam/pockedox/data/PokedexService.kt
@@ -1,7 +1,10 @@
 package com.epam.pockedox.data
 
+import com.epam.pockedox.domain.PokemonInfoResponse
+import com.epam.pockedox.domain.PokemonListResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PokedexService {
@@ -10,4 +13,7 @@ interface PokedexService {
         @Query("limit") limit : Int = 30,
         @Query("offset") offset : Int = 0
     ) : Call<PokemonListResponse>
+
+    @GET("pokemon/{name}")
+    fun getPokemonById(@Path("name") name: String): Call<PokemonInfoResponse>
 }
